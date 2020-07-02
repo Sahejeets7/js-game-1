93f14db49404dd72b6825f0f461c11c61c2ac41f
sahz <Sahejeetsingh97@gmail.com>
2020-07-02 10:55:02 +0530

    1st commit for 1st js game

---
+++ app.js
@@ -0,0 +1,19 @@
+//Prepare the Array storing items of the Grid
+grid = [
+    {
+        name: 'Pizza',
+        img: './images/pizza.png'
+        
+    },
+
+];
+
+
+
+
+
+function makegrid(){
+    for(let i = 0; i<= grid.length(); i++){
+
+    }
+}
